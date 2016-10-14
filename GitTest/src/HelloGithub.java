16a5b4309208f5d9ef016c14f4701f01b1e480a9
YEAH1 <YEAH1@192.168.0.10>
2016-10-14 19:49:24 +0900

    git test 수

--- GitTest/src/HelloGithub.java
+++ GitTest/src/HelloGithub.java
@@ -4,6 +4,8 @@ public class HelloGithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github");
+		
+		System.out.println("수정후 push");
 
 	}
 
